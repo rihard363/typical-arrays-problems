9a8adf332addbf9a223f76906e9a1f32a43669e3
rihard363 <stragking6@gmail.com>
2021-03-06 12:46:19 +0300

    init: start task and end typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = function min(numArr) {
+  console.log("просто строка");
+  if (numArr === undefined || numArr.length === 0) {
+    return 0;
+  } else {
+    return Math.min.apply(null, numArr);
+  }
+};
+exports.max = function max(numArr) {
+  console.log("просто строка");
+    if (numArr === undefined || numArr.length === 0) {
+        return 0;
+      } else {
+    return Math.max.apply(null, numArr);
+  };
+};
+exports.avg = function avg(numArr) {
+  console.log("просто строка");
+  if (numArr === undefined || numArr.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    sum = sum + numArr[i];
+  };
+  return sum / numArr.length;
+};
